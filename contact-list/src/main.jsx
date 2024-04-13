c090da9f02f3ba02fb344da16ab67bfe4445ba7a
ksaraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2024-04-13 03:03:21 -0400

    client side routing
    -- creating a contact form
    -- routing to different endpoints

--- contact-list/src/main.jsx
+++ contact-list/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import './index.css'
 import Root from './routes/root'
+import Contact from "./routes/contact"
 import ErrorPage from './error-page'
 
 const router = createBrowserRouter([
@@ -10,7 +11,13 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
-  }
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />
+      },
+    ],
+  },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
